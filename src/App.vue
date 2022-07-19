caca53c94355b7ad72b7d113de839463065966f7
jmerazo <jmerazo96@gmail.com>
2022-07-19 16:01:45 -0500

    Views and others

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <menu-top></menu-top>    
+  <router-view></router-view>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import menuTop from './components/menuTop.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    menuTop
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/menuTop.vue
@@ -0,0 +1,78 @@
+<template>
+  <header class="header">
+    <div class="">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="#"
+            ><img
+              src="@/assets/resources/queue.png"
+              width="50"
+              height="50"
+            />Queue</a
+          >
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav w-100 justify-content-center">
+              <a
+                class="nav-item nav-link active"
+                href=""
+                style="color: #1f4181"
+                >Inicio
+              </a>
+              <a
+                class="nav-item nav-link"
+                href=""
+                style="color: #1f4181"
+                >IngresoCO</a
+              >
+              <a
+                class="nav-item nav-link"
+                href=""
+                style="color: #1f4181"
+                >Devoluciones</a
+              >
+              <a
+                class="nav-item nav-link"
+                href=""
+                style="color: #1f4181"
+                >Consulta</a
+              >
+              <a class="nav-item nav-link" href="/Imagen" style="color: #1f4181"
+                >Imagenes</a
+              >
+              <a
+                class="nav-item nav-link"
+                href=""
+                style="color: #1f4181"
+                >Registro</a
+              >
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "menuTop"
+}
+</script>
+<style>
+.header {
+  background-color: #F3F9F7;
+  border-bottom: 1px solid #81718B;
+  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+}
+</style>
