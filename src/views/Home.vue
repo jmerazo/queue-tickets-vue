ed70b9f55eb728e972e4ecc2b17f4730d6402b23
jmerazo <jmerazo96@gmail.com>
2022-07-21 17:00:54 -0500

    Create person

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,20 @@
         <section class="index">
             <h1 class="display-1" style="text-align:center; ">Tickets</h1>
             <div style="text-align:center;">
-                <label for="">Fill out the form</label><br>
-                <label for="">Document type</label><input type="text" class="form-ticket-1"><br>
-                <label for="">Document number</label><input type="text" class="form-ticket-1"><br>
-                <label for="">Names</label><input type="text" class="form-ticket-1"><br>
-                <label for="">Last Names</label><input type="text" class="form-ticket-1"><br>
-                <label for="">Phone</label><input type="text" class="form-ticket-1"><br>
-                <label for="">Email</label><input type="text" class="form-ticket-1"><br>
+                <label>Fill out the form</label><br><br>
+                <label>Document type</label><input v-model="person.document_type" type="text" class="form-ticket-1"><br>
+                <label>Document number</label><input v-model="person.document_number" type="text" class="form-ticket-1"><br>
+                <label>Names</label><input v-model="person.names" type="text" class="form-ticket-1"><br>
+                <label>Last Names</label><input v-model="person.last_names" type="text" class="form-ticket-1"><br>
+                <label>Phone</label><input v-model="person.phone" type="text" class="form-ticket-1"><br>
+                <label>Email</label><input v-model="person.email" type="text" class="form-ticket-1"><br>
 
                 <div class="col-6">
                   <div class="form-group">
                       <label for="departments">Departments</label>
-                      <select ref="selectDepartment" v-model="this.departments" class="form-control" id="department">
-                          <option v-for="department in departments" :value="department.id" :key="department">{{department.name}}</option>
+                      <select v-model="selDepartment" @change="listCities()" class="form-control" id="department">
+                          <option value="" disabled>Select an option...</option>
+                          <option v-for="department in departments" :value="department" :key="department.code">{{department.name}}</option>
                       </select>
                   </div>
                 </div>
@@ -23,12 +24,13 @@
                 <div class="col-6">
                   <div class="form-group">
                       <label for="cities">Cities</label>
-                      <select ref="selectCity" v-model="this.cities" class="form-control" id="city">
+                      <select v-model="selCity" class="form-control" id="city">
+                          <option value="" disabled>Select an option...</option>
                           <option v-for="city in cities" :value="city.id" :key="city">{{city.name}}</option>
                       </select>
                   </div>
                 </div>
-                <button style="margin-left:0.5rem; margin-top:10px" class="btn btn-secondary" type="button" @click="salir">Send</button>
+                <button class="btn btn-secondary" @click="createPerson">Send</button>
             </div>
         </section>
     </section> 
@@ -43,36 +45,73 @@ export default {
   name: "Home-AQ",
   data() {
     return {
+      person: {
+        document_type: "",
+        document_number: "",
+        names: "",
+        last_names: "",
+        phone: "",
+        email: ""
+      },
       departments: [],
       cities: [],
-      select: this.$refs.selectDepartment
+      selDepartment: "",
+      selCity: ""
     };    
   },
   mounted() {
-    axios.get(`http://localhost:8888/apitickets/cities/filter/${this.select}`, {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      }
-    })
-    .then((Response) => {
-      console.log("Cities", Response.data)
-      this.cities = Response.data;
-    });
-
-    axios.get("http://localhost:8888/apitickets/departments", {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      }
-    })
-    .then((Response) => {
-      console.log("Departments", Response.data)
-      this.departments = Response.data;
-    });
+    this.listDepartments();
+    this.listCities();  
   },
   methods: {
-    
+    async listCities() {
+      if(!this.selDepartment){
+        this.cities = []
+      }else{
+        await axios.get(`http://localhost:8888/apitickets/cities/filter/${this.selDepartment.code}`, {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          }
+        })
+        .then((Response) => {
+          //console.log("Cities", Response.data)
+          this.cities = Response.data;
+        });
+      }
+    },
+    async listDepartments(){
+      await axios.get("http://localhost:8888/apitickets/departments", {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then((Response) => {
+        //console.log("Departments", Response.data)
+        this.departments = Response.data;
+      });
+    },
+    async createPerson() {
+      let person = new FormData();
+      person.append("document_type", this.person.document_type);
+      person.append("document_number", this.person.document_number);
+      person.append("names", this.person.names);
+      person.append("last_names", this.person.last_names);
+      person.append("phone", this.person.phone);
+      person.append("email", this.person.email);
+      person.append("city_id", this.selCity);
+      person.append("department_id", this.selDepartment.code);
+
+      await axios.post("http://localhost:8888/apitickets/person/create", person, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(() => {
+        this.$router.push("/");
+      });
+    }    
   }
 };
 </script>
